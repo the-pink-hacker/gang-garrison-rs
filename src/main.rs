cbb64e6c57c7bce8c6485e8fb552a7bb2edba608
Pink Garrett <pink@thepinkhacker.com>
2024-08-28 00:34:24 -0400

    Setup bevy

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
-fn main() {
+use bevy::prelude::*;
+
+fn test() {
     println!("Hello, world!");
+}
+
+fn main() {
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_systems(Update, test)
+        .run();
 }
