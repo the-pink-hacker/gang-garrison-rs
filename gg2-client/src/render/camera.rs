3121f03a24f1c6b2fd33288ee0f604df2bd0545b
Pink Garrett <pink@thepinkhacker.com>
2024-10-13 21:50:11 -0400

    Run move camera systems on normal update schedule

--- gg2-client/src/render/camera.rs
+++ gg2-client/src/render/camera.rs
@@ -1,6 +1,6 @@
 use bevy::{input::common_conditions::input_pressed, prelude::*};
 
-const MOVE_SPEED: f32 = 8.0;
+const MOVE_SPEED: f32 = 400.0;
 
 #[derive(Component)]
 pub struct MainCamera;
@@ -9,27 +9,27 @@ fn setup_system(mut commands: Commands) {
     commands.spawn((MainCamera, Camera2dBundle::default()));
 }
 
-fn move_camera_down_system(mut query: Query<&mut Transform, With<MainCamera>>) {
+fn move_camera_down_system(mut query: Query<&mut Transform, With<MainCamera>>, time: Res<Time>) {
     if let Ok(mut camera_transform) = query.get_single_mut() {
-        camera_transform.translation += Vec3::NEG_Y * MOVE_SPEED;
+        camera_transform.translation += Vec3::NEG_Y * MOVE_SPEED * time.delta_seconds();
     }
 }
 
-fn move_camera_up_system(mut query: Query<&mut Transform, With<MainCamera>>) {
+fn move_camera_up_system(mut query: Query<&mut Transform, With<MainCamera>>, time: Res<Time>) {
     if let Ok(mut camera_transform) = query.get_single_mut() {
-        camera_transform.translation += Vec3::Y * MOVE_SPEED;
+        camera_transform.translation += Vec3::Y * MOVE_SPEED * time.delta_seconds();
     }
 }
 
-fn move_camera_left_system(mut query: Query<&mut Transform, With<MainCamera>>) {
+fn move_camera_left_system(mut query: Query<&mut Transform, With<MainCamera>>, time: Res<Time>) {
     if let Ok(mut camera_transform) = query.get_single_mut() {
-        camera_transform.translation += Vec3::NEG_X * MOVE_SPEED;
+        camera_transform.translation += Vec3::NEG_X * MOVE_SPEED * time.delta_seconds();
     }
 }
 
-fn move_camera_right_system(mut query: Query<&mut Transform, With<MainCamera>>) {
+fn move_camera_right_system(mut query: Query<&mut Transform, With<MainCamera>>, time: Res<Time>) {
     if let Ok(mut camera_transform) = query.get_single_mut() {
-        camera_transform.translation += Vec3::X * MOVE_SPEED;
+        camera_transform.translation += Vec3::X * MOVE_SPEED * time.delta_seconds();
     }
 }
 
@@ -38,7 +38,7 @@ pub struct CameraPlugin;
 impl Plugin for CameraPlugin {
     fn build(&self, app: &mut App) {
         app.add_systems(Startup, setup_system).add_systems(
-            FixedUpdate,
+            Update,
             (
                 move_camera_down_system.run_if(input_pressed(KeyCode::KeyS)),
                 move_camera_up_system.run_if(input_pressed(KeyCode::KeyW)),
